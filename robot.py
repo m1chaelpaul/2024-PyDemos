1e0a5270407422abe2ab4d19ad6b0d71335a22ff
Combustible Lemons <lemons@frc5113.com>
2024-02-28 15:11:49 -0500

    some changes lol

--- robot.py
+++ robot.py
@@ -15,4 +15,21 @@ class MyRobot(magicbot.MagicRobot):
         self.fl_motor = phoenix5.WPI_TalonSRX(12)
         self.br_motor = phoenix5.WPI_TalonSRX(15)
         self.bl_motor = phoenix5.WPI_TalonSRX(18)
+
+        self.rightMotors = wpilib.MotorControllerGroup(
+            self.fr_motor, self.br_motor
+        )
+
+        self.leftMotors = wpilib.MotorControllerGroup(
+            self.bl_motor, self.br_motor
+        )
+
+        self.robDrive = wpilib.drive.DifferentialDrive(
+            self.rightMotors, self.leftMotors
+        )
+        
         self.controller = wpilib.Joystick(8)
+
+
+    def teleopPeriodic(self):
+        self.rd
