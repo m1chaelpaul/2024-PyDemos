8c5f29122694c2d820769bc7a1b5eabe6ca15a1b
Combustible Lemons <lemons@frc5113.com>
2024-02-28 15:16:12 -0500

    v6

--- robot.py
+++ robot.py
@@ -16,20 +16,11 @@ class MyRobot(magicbot.MagicRobot):
         self.br_motor = phoenix5.WPI_TalonSRX(15)
         self.bl_motor = phoenix5.WPI_TalonSRX(18)
 
-        self.rightMotors = wpilib.MotorControllerGroup(
-            self.fr_motor, self.br_motor
-        )
-
-        self.leftMotors = wpilib.MotorControllerGroup(
-            self.bl_motor, self.br_motor
-        )
-
-        self.robDrive = wpilib.drive.DifferentialDrive(
-            self.rightMotors, self.leftMotors
-        )
-        
+        self.rightMotors = wpilib.MotorControllerGroup(self.fr_motor, self.br_motor)
+        self.leftMotors = wpilib.MotorControllerGroup(self.bl_motor, self.br_motor)
+        self.robDrive = wpilib.drive.DifferentialDrive(self.rightMotors, self.leftMotors)
         self.controller = wpilib.Joystick(8)
 
 
     def teleopPeriodic(self):
-        self.rd
+        self.robDrive.arcadeDrive(self.controller.getY(), self.controller.getX())
