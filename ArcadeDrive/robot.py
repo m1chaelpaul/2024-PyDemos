58e08b9e1c0e6344d7c068222126e53022a31982
Combustible Lemons <lemons@frc5113.com>
2024-03-01 14:12:47 -0500

    changed joystick id

--- ArcadeDrive/robot.py
+++ ArcadeDrive/robot.py
@@ -10,7 +10,7 @@ class MyRobot(wpilib.TimedRobot):
         self.fl_motor = phoenix5.WPI_TalonSRX(12)
         self.br_motor = phoenix5.WPI_TalonSRX(15)
         self.bl_motor = phoenix5.WPI_TalonSRX(18)
-        self.Joystick = wpilib.Joystick(8)
+        self.Joystick = wpilib.Joystick(0)
 
         #create motor group
         self.rm = wpilib.MotorControllerGroup(self.fr_motor, self.br_motor)
