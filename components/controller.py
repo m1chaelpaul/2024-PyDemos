adc453f302d6dc40dbc6a882490df0f7d02b38c2
Combustible Lemons <lemons@frc5113.com>
2024-03-04 14:05:50 -0500

    v8

---
+++ components/controller.py
@@ -0,0 +1,38 @@
+import wpimath.controller
+import magicbot
+from magicbot.state_machine import state
+from magicbot import tunable
+import navx
+
+from components.drivetrain import Drivetrain
+
+class Controller(magicbot.StateMachine):
+
+    drivetrain: Drivetrain
+    navx: navx.AHRS
+
+    turn_to_angle_kP = tunable(0.03)
+    turn_to_angle_kI = tunable(0)
+    turn_to_angle_kD = tunable(0)
+
+    def setup(self):
+        self.turn_to_angle_controller = wpimath.controller.PIDController(self.turn_to_angle_kP, self.turn_to_angle_kI, self.turn_to_angle_kD)
+        self.turn_to_angle_controller.enableContinuousInput(0, 360)
+
+    def setAngle(self, angle:float):
+        self.turn_to_angle_controller.setSetpoint(angle)
+
+    def turn_to_angle(self):
+        self.engage(initial_state="turning_to_angle")
+
+    def turn_to_angle(self, angle:float):
+        self.setAngle(angle)
+        self.engage(initial_state="turning_to_angle")
+
+    @state(first=True)
+    def turning_to_angle(self):
+        self.turn_to_angle_controller.setPID(self.turn_to_angle_kP, self.turn_to_angle_kI, self.turn_to_angle_kD)
+
+        measurement = self.navx.getAngle()
+        output = self.turn_to_angle_controller.calculate(measurement)
+        self.drivetrain.arcadeDrive(0, (output, -0.3, -0.3))
--- robot.py
+++ robot.py
@@ -4,11 +4,15 @@ import wpilib.drive
 import phoenix5
 from components.drivetrain import Drivetrain
 from components.joystick import Joystick
+from components.controller import Controller
+from wpilib import SmartDashboard
+import navx
 
 class MyRobot(magicbot.MagicRobot):
 
     drivetrain: Drivetrain
     joystick: Joystick
+    controller: Controller
 
     def createObjects(self):
         self.fr_motor = phoenix5.WPI_TalonSRX(55)
@@ -19,7 +23,19 @@ class MyRobot(magicbot.MagicRobot):
         self.rightMotors = wpilib.MotorControllerGroup(self.fr_motor, self.br_motor)
         self.leftMotors = wpilib.MotorControllerGroup(self.bl_motor, self.br_motor)
         self.robDrive = wpilib.drive.DifferentialDrive(self.rightMotors, self.leftMotors)
+        self.navx = navx.AHRS.create_spi()
         self.controller = wpilib.Joystick(0)
 
+
     def teleopPeriodic(self):
-        self.robDrive.arcadeDrive(self.controller.getY(), self.controller.getX())
+        try:
+            if self.joystick.getTrigger():
+                self.controller.turn_to_angle(180)
+            else:
+                self.drivetrain.arcadeDrive(self.controller.getY(), self.controller.getX())
+                
+        except:
+            self.onException()
+
+        SmartDashboard.putNumber("JOYSTICK X: ", self.controller.getX())
+        SmartDashboard.putBoolean("JOYSTICK Y:", self.controller.getY())
